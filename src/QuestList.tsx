44022a88f8be24b318f716954b0305674301decb
Chackbubu <wassmuthp@gmail.com>
2024-05-31 20:05:18 +0200

    QuestList added

--- src/QuestList.tsx
+++ src/QuestList.tsx
@@ -2,48 +2,44 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface Quest {
-	nodes: Node[];
-	name: string;
-	current: number;
-	max: number;
-	// Add more properties for detailed information
+    name: string;
+    current: number;
+    max: number;
+    // Add more properties for detailed information
 }
 
 interface DropdownProps {
-	quests: Quest[];
+    quests: Quest[];
 }
 
 const Dropdown: React.FC<DropdownProps> = ({ quests }) => {
-	const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<div className="Dropdown">
-			<button className="Dropdown-button" onClick={() => setIsOpen(!isOpen)}>
-				{isOpen ? 'Close Quests' : 'Quests'}
-			</button>
+    return (
+        <div className="Dropdown">
+            <button className="Dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? 'Close Quests' : 'Quests'}
+            </button>
 
-			<ul className={`Dropdown-content ${isOpen ? 'show' : ''}`}>
-				{quests.map((quest, index) => (
-					<li key={index} className="Dropdown-item">
-						<Link to={`/quest/${quest.name}`}>
-							<div className="quest-container">
-								<div className="quest-text">
-									{quest.name}
-									<br />
-								</div>
-								<div className="progress-container">
-									<progress value={quest.current} max={quest.max} />
-									<span className="progress-label">
-										{quest.current}/{quest.max}
-									</span>
-								</div>
-							</div>
-						</Link>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+            <ul className={`Dropdown-content ${isOpen ? 'show' : ''}`}>
+                {quests.map((quest, index) => (
+                    <li key={index} className="Dropdown-item">
+                        <Link to={`/quest/${quest.name}`}>
+                            <div className="quest-container">
+                                <div className="quest-text">
+                                    {quest.name}<br />
+                                </div>
+                                <div className="progress-container">
+                                    <progress value={quest.current} max={quest.max} />
+                                    <span className="progress-label">{quest.current}/{quest.max}</span>
+                                </div>
+                            </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Dropdown;
