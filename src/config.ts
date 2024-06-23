4b4a6aad2f96781bf8e0e02a287216fa96c776a5
pilip-d <philipdell@gmx.de>
2024-06-23 22:38:01 +0200

    reduce search radius

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const MAX_DIST_TO_NODE = 50; //20;
+export const MAX_DIST_TO_NODE = 20; //20;
 export const SEARCH_RADIUS = 1000;
 export const MIN_OCCURENCES_FOR_QUEST = 5;
 export const MAX_OCCURENCES_FOR_QUEST = 30;
